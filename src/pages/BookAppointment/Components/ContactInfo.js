1995104e7c8da7d8e45eb7d535c4b99069c2c28c
allistairyu <78183297+allistairyu@users.noreply.github.com>
2021-09-04 22:19:19 -0700

    autofocus on First Name field

--- src/pages/BookAppointment/Components/ContactInfo.js
+++ src/pages/BookAppointment/Components/ContactInfo.js
@@ -10,7 +10,7 @@ export default function ContactInfo({ handlePhoneNumChange, handleChange, values
 
     return (
         <form autoComplete='off' >
-            <TextField label="First Name" name='firstName' onChange={handleChange} error={values.errors.firstName} defaultValue={values.firstName} />
+            <TextField autoFocus label="First Name" name='firstName' onChange={handleChange} error={values.errors.firstName} defaultValue={values.firstName} />
             <TextField label="Last Name" name='lastName' onChange={handleChange} error={values.errors.lastName} defaultValue={values.lastName} />
             <br></br><br></br>
             <MuiPhoneNumber className='phoneNum' defaultCountry='us' disableAreaCodes onChange={handlePhoneNumChange} error={values.errors.phoneNumber} value={values.phoneNumber} />
