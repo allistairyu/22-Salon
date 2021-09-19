c07476a990b6f43da8a82be5ee26dc10568a3711
allistairyu <78183297+allistairyu@users.noreply.github.com>
2021-09-18 21:26:09 -0500

    calendar and appointments rendered if user is logged in

--- src/pages/AdminView/index.js
+++ src/pages/AdminView/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import Navbar from '../App/Components/Navbar'
 import UserContainer from '../App/Components/UserContainer'
 import Calendar from 'react-awesome-calendar';
+import submitLogin from './Login'
+import submitRegister from './Register'
 var moment = require('moment-timezone');
 
 //TODO: event calendar, delete appointment confirmation dialog, change hours,
@@ -10,47 +12,27 @@ export default function index() {
 
     const [username, setUsername] = useState()
     const [password, setPassword] = useState()
-
     const [events, setEvents] = useState([])
-
-    // [{
-    //     id: 1,
-    //     color: '#fd3153',
-    //     from: '2021-09-08T18:00:00+00:00',
-    //     to: '2021-09-08T18:30:00+00:00',
-    //     title: 'This is an event'
-    // }]
-
     const [loginRegister, toggleLoginRegister] = useState(true)
+    const [loggedIn, setLoggedIn] = useState(false)
     
-    useEffect(async () => {
-        let response = await fetch('/api/appointments')
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        const json = await response.json()
+    useEffect(() => {
+        async function verifyToken() {
+            let response = await fetch('/api/appointments')
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            const json = await response.json()
 
-        for (const [index, appointment] of json.entries()) {
-            let dateTime = convertToISO(appointment.date, appointment.time)
-            setEvents(events => [...events, {id: index, color: '#fd3153', from: dateTime, to: dateTime, title: appointment.firstName}])
+            for (const [index, appointment] of json.entries()) {
+                let dateTime = convertToISO(appointment.date, appointment.time)
+                setEvents(events => [...events, {id: index, color: 'red', from: dateTime, to: dateTime, title: appointment.firstName}])
+            }
+            renderAppointments()
         }
+        verifyToken()
     }, [])
-    //TODO: KEEP?
-    // useEffect(() => {
-    //     function checkUserData() {
-    //         const item = localStorage.getItem('token')
-        
-    //         if (item) {
-    //             return <UserContainer />
-    //         }
-    //     }
-        
-    //     window.addEventListener('storage', checkUserData)
-        
-    //     return () => {
-    //         window.removeEventListener('storage', checkUserData)
-    //     }
-    // }, [])
+
     //TODO: make not sketchy, convert to local time?
     const convertToISO = (date, time) => {
         let ISOdate = new Date(date).toISOString()
@@ -66,84 +48,61 @@ export default function index() {
         // return moment.tz(ISOdate, "America/Los_Angeles").format()
     }
 
-    const submitLogin = async () => {
-        try {
-            const response = await fetch('/api/login', {
-                method: 'POST',
-                body: JSON.stringify({username: username, password: password}),
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            })
-            if (response.status === 204) {
-                alert('bad credentials')
-                return
-            }
-			const json = await response.json()
-            localStorage.setItem('token', json.token)
-            localStorage.setItem('username', json.username)
-
-        } catch (e) {
-            console.log(e)
-        }
-    }
-    const submitRegister = async () => {
-        try {
-            const response = await fetch('/api/register', {
-                method: 'POST',
-                body: JSON.stringify({username: username, password: password}),
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            })
-            if (!response.ok) {
-				throw Error(response.statusText);
-			}
-			const json = await response.json()
-            localStorage.setItem('token', json.token)
-        } catch (e) {
-            console.log(e)
-        }
-    }
-
     const switchLoginRegister = (e) => {
         e.preventDefault()
         toggleLoginRegister(loginRegister => !loginRegister)
     }
 
-    //TODO: add verifyToken?
-    const renderAppointments = () => {
-        if (localStorage.getItem('token')) return (
-            <div>
-                <Calendar events={events} />
-                <UserContainer />
-            </div>
-        )
+    const renderAppointments = async () => {
+        const token = localStorage.getItem('token')
+        if (token && localStorage.getItem('username')) {
+            try {
+                let response = await fetch('/api/admin', {
+                    headers: {username: localStorage.getItem('username'), token}
+                })
+                response = await response.json()
+            } catch (e) {
+                alert('invalid')
+            }
+            setLoggedIn(true)
+        }
+    }
+
+    const logOut = () => {
+        localStorage.removeItem('username')
+        localStorage.removeItem('token')
+        window.location.reload()
     }
 
     return (
             <div>
                 <Navbar />
                 <div className='navbar-margin'></div>
-                {loginRegister ? 'Log In' : 'Register' }
-                <form onSubmit={loginRegister ? submitLogin : submitRegister}>
-                    <label>
-                        username: 
-                        <input type='text' onChange={e => setUsername(e.target.value)} />
-                    </label>
-                    <br></br><br></br>
-                    <label>
-                        password: 
-                        <input type='password' onChange={e => setPassword(e.target.value)} />
-                    </label>
-                    <br></br><br></br>
-                    <input type="submit" value="Submit" />
-                    <br></br>
-                    <button onClick={e => switchLoginRegister(e)}>{loginRegister ? 'Register Instead' : 'Log In Instead'}</button>
-
-                </form>
-                {renderAppointments()}
-                {/* <Calendar events={events} /> */}
+                {loggedIn ? 
+                    <div>
+                        <button onClick={logOut}>Log Out</button>
+                        <Calendar events={events} />
+                        <UserContainer />
+                    </div> :
+                    <div>
+                        {loginRegister ? 'Log In' : 'Register'}
+                        <form onSubmit={() => loginRegister ? submitLogin({username, password}, setLoggedIn) : submitRegister({username, password})}>
+                            <label>
+                                username: 
+                                <input type='text' onChange={e => setUsername(e.target.value)} />
+                            </label>
+                            <br></br><br></br>
+                            <label>
+                                password: 
+                                <input type='password' onChange={e => setPassword(e.target.value)} />
+                            </label>
+                            <br></br><br></br>
+                            <input type="submit" value="Submit" />
+                            <br></br>
+                            <button onClick={e => switchLoginRegister(e)}>{loginRegister ? 'Register Instead' : 'Log In Instead'}</button>
+                        </form>
+                    </div>
+                }
             </div>
     )
 }
